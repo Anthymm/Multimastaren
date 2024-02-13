e597ea898d441ad8ff9754f9468757d1aa4b3525
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-02-13 20:55:14 +0100

    fixade bug i TestView.vue

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,29 +1,28 @@
 <script>
-
-
 export default {
     created() {
-        this.createTable(1,this.timesOne)
+        this.createTable(1, this.timesOne);
+        console.log(this.timesOne)
     },
     data() {
         return {
             timesOne: [],
-            i: 0
-        }
+        };
     },
     methods:{
-        createTable(x,y){
-            while (i < 11) {
-                y.push(i * x)
-                i++;
+        createTable(x, y){
+            for (let i = 0; i <= 10; i++) {
+                y.push(i * x);
             }
-        }
-    }
-}
-console.log(timesOne)
+        },
+    },
+};
 </script>
+
 <template>
     TestView
 </template>
+
 <style>
 </style>
+
