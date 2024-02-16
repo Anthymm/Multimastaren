9f11c8159b9148fbf4bbaa7e5e7d4d43832b75e4
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-02-16 11:40:10 +0100

    ändrat generering av tabeller

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,51 +1,81 @@
 <script>
+import { getTransitionRawChildren } from "vue";
 import HomeBtn from "../components/HomeBtn.vue"
 export default {
     components: {
         HomeBtn
-
     },
     created() {
-        // this.createTable(1, this.timesOne);
-        // console.log(this.timesOne)
-        this.createTable();
-        console.log(this.tables)
+        this.generateTables();
+        // this.createTable();
+        // this.selectArr();
+        // console.log(this.selectedQuestion[this.selectedIndex], this.selectedArr[this.selectedIndex])
+        // console.log(`randomNumber = ${this.randomNumber}, selectedIndex = ${this.selectedIndex}`)
+        // console.log(`randomNumber- väljer arr från tables + questions`)
+        // console.log(`selectedIndex- väljer index i valda arr `)
+
+        // console.log(tables[0][1])
+        console.log(this.tables[0][0].question)
+        console.log(this.tables[0][0].answer)
+        console.log(this.randomNumber, this.randomNumber2)
     },
     data() {
         return {
             // timesOne: [],
-            // timesTwo: [],
-            // timesThree: [],
-            // timesFour: [],
-            // timesFive: [],
-            // timesSix: [],
-            // timesSeven: [],
-            // timesEight: [],
-            // timesNine: [],
-            // timesTen: [],
-            tables: {}
+            // tables: {},
+            // questions: {},
+            randomNumber: Math.floor(Math.random() * 10),
+            randomNumber2: Math.floor(Math.random() * 10),
+            // selectedArr: null,
+            // selectedQuestion: null,
+            // selectedIndex: 5,
+            tables: []
         };
     },
     methods: {
-        createTable() {
-            let emptyArray = []
-
+        generateTables() {
             for (let i = 1; i <= 10; i++) {
-                for (let j = 1; j <= 10; j++) {
-                    emptyArray.push(i * j)
-                }
-                this.tables["times" + i] = emptyArray
-                emptyArray = []
+            const table = [];
+            for (let j = 1; j <= 10; j++) {
+                table.push({ question: `${i} x ${j} =`, answer: i * j });
             }
-
+            this.tables.push(table);
+            }
         },
+
+        // createTable() {
+        //     let temporaryAnswer = []
+        //     let temporaryQuestion = []
+
+        //     for (let i = 1; i <= 5; i++) {
+        //         for (let j = 1; j <= 10; j++) {
+        //             temporaryAnswer.push(i * j)
+        //             temporaryQuestion.push(i + '*' + j + '=')
+        //         }
+        //         this.tables["times" + i] = temporaryAnswer
+        //         this.questions["times" + i] = temporaryQuestion
+        //         temporaryAnswer = []
+        //         temporaryQuestion = []
+        //     }
+
+        // },
+        // selectArr(){
+        //     this.selectedArr = this.tables["times" + this.randomNumber]
+        //     this.selectedQuestion = this.questions["times" + this.randomNumber]
+        // }
     },
 };
 </script>
 
 <template>
-    <HomeBtn />
+    <div>
+        <HomeBtn/>
+        <h1 class="text">{{ this.tables[this.randomNumber][this.randomNumber2].question}} {{ this.tables[this.randomNumber][this.randomNumber2].answer}}</h1>
+    </div>
 </template>
 
 <style>
+.text{
+    color: white
+}
 </style>
