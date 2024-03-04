fd5ba57c2d20bc5bc31f0f26633a2493d68a17e0
n0llacoo <sami.fari@iths.se>
2024-03-04 11:02:57 +0100

    uppdaterat design

--- src/components/RestartTest.vue
+++ src/components/RestartTest.vue
@@ -11,7 +11,7 @@ export default {};
 }
 #restartBtn{
   color: #955a00;
-  font-size: 1.5rem;
+  font-size: 1.85rem;
   text-decoration: none !important;
   background-color: #bcd7e7;
   padding: 0.5rem 2rem;
@@ -20,6 +20,9 @@ export default {};
 }
 #restartBtn:hover {
   text-decoration: none;
-  font-size: 1.7rem;
+  font-size: 1.9rem;
+ background-color: #ffb74b;
+ color:#2988be;
 }
 </style>
+#ffb74b;
